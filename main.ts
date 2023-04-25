fbf4378c6c0a384f7d36b34623558717168f7a44
martinibeverage <122539603+martinibeverage@users.noreply.github.com>
2023-04-25 08:31:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,20 @@ input.onButtonPressed(Button.A, function () {
     comment.comment("Sets the fork to the \"Down\" position")
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 })
+input.onButtonPressed(Button.AB, function () {
+    comment.comment("Moves forward for 1 second")
+    wuKong.setAllMotor(20, 20)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.B, function () {
+    comment.comment("Sets the fork to the \"Up\" position")
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    comment.comment("Moves backward for 1 second")
+    wuKong.setAllMotor(-20, -20)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
 basic.showIcon(IconNames.Rollerskate)
